a54c5ca2cab11f3be5fc79d4f6701b57a2cda60e
rahulpa <rahulpa@google.com>
2019-06-28 09:58:09 -0700

    modify run client script to take executable and instance count - e.g ./run_many_client.sh /tmp/client_v12 100

--- grpc/run_many_client.sh
+++ grpc/run_many_client.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-go build -o /tmp/client helloworld_client/main.go
-
-num=$1
+num=$2
+client=$1
+name=`basename $client`
+echo $name
 i=0
 while [ $i -le $num ] ; do
-    /tmp/client &
+    log="/tmp/${name}_$i.log"
+    rm -rf $log
+    $client >& $log &
     pid=$!
     pids[${i}]=$!
     i=`expr $i + 1`
